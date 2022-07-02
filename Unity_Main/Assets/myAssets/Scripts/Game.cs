43f79f3d035dc06b5b79b31350f7d11caf47ebf7
Gabriel99999999 <wiedermann.gabriel@gmail.com>
2022-07-02 18:17:56 +0200

    prefabs und button
    prefabs angelegt plus spawnen lassen und Button angelegt

---
+++ Unity_Main/Assets/myAssets/Scripts/Game.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    private bool turn; //true = player1, false = player2
+
+//Konstruktor
+    public Game(){
+        print("Game created");
+        //Player1 = new Player();
+        //Player2 = new Player();
+        turn = true;
+    }
+//Destuktor
+    ~Game(){
+        print("Game deleted");
+    }
+//public Methods 
+    public bool CheckWin(){
+        bool win = false;
+
+        return win;
+    }
+
+//private Variables
+    private Player Player1;
+    private Player Player2;
+    private Grid Map;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Game Spiel = gameObject.AddComponent<Game>(); //evtl noch ändern
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //the win condition should get use here
+        /*while (true)
+        {
+            if (turn)
+            {
+                //player1 can do whatever he wants
+
+
+                //reset the values of the units
+                //Player1.ResetUnits();
+            }
+            else
+            {
+                //player2 can do whatever he wants
+
+                //reset the values of the units
+            }
+        }*/
+        
+    }
+
+    public void Buy()
+    {
+        int choice = 0;
+        if(turn == true)
+        {
+            //muss noch in ein menü embedded werden
+            /*choose what to buy:
+             *  display player1 money
+             *  display all affordable options
+             *  each option is represented by an int
+             */
+            Player1.AddUnit(choice);
+        }
+        else
+        {
+            //das selbe mit player2
+            Player2.AddUnit(choice);
+        }
+    }
+
+    //Function for the Round button
+    public void ClickTest()
+    {
+        //turn = !turn;
+        Debug.Log("Button geklickt");
+       // Debug.Log(turn);
+    }
+}
---
+++ Unity_Main/Assets/myAssets/Scripts/Player.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //von Gabs ab hier test bis zum kommentar Konstruktor
+    public GameObject infantryPrefab;
+    public GameObject cavalryPrefab;
+    public GameObject archerPrefab;
+    public GameObject rangePrefab;
+
+    //Konstruktor
+    public Player(){
+        //army.Add(new Infantry());
+        //army.Add(new Infantry());
+        //army.Add(new Infantry());
+        print("player was created");
+        money = 3;
+    }
+//Destruktor
+    ~Player(){
+        print("player was deleted");
+    }
+//public Methods
+    public void PlaceUnit(){
+        //Versuch um gleich prefabs hinzuzufügen:
+        //https://www.youtube.com/watch?v=eACZk-XDP2A
+        Instantiate(infantryPrefab, transform.position, Quaternion.identity);
+
+        /* das war schon vorher da
+        GameObject infObject = new GameObject("Infantry-1");
+        infObject.transform.parent = this.transform;
+        infObject.AddComponent<Infantry>();
+        GameObject rangeObject = new GameObject("Archer-1");
+        rangeObject.transform.parent = this.transform;
+        rangeObject.AddComponent<Range>();
+        */
+
+        //das war schon auskommentiert
+        /*GameObject cavObject = new GameObject("Cavalry-1");
+        cavObject.transform.parent = this.transform;
+        cavObject.AddComponent<Cavalry>();
+        GameObject siegeObject = new GameObject("Siege-1");
+        siegeObject.transform.parent = this.transform;
+        siegeObject.AddComponent<Siege>();*/
+
+        //das war nicht auskommentiert
+        /*army.Add(infObject);
+        army.Add(rangeObject);*/
+    }
+
+    public void AddUnit(int choice)
+    {
+        GameObject newObject;
+        switch (choice)
+        {
+            case 0:
+                newObject = new GameObject("Infantry-1");
+                newObject.AddComponent<Infantry>();
+                money -= 5;
+                break;
+            case 1:
+                newObject = new GameObject("Archer-1");
+                newObject.AddComponent<Range>();
+                money -= 7;
+                break;
+            case 2:
+                newObject = new GameObject("Cavalry-1");
+                newObject.AddComponent<Cavalry>();
+                money -= 10;
+                break;
+            default:
+                newObject = new GameObject("Infantry-1");
+                newObject.AddComponent<Infantry>();
+                money -= 5;
+                break;
+        }
+        newObject.transform.parent = this.transform;
+        army.Add(newObject);
+    }
+    //this function should reset the values of each unit a player has when he/she ends his/her turn
+    public void ResetUnits()
+    {
+        //army.Count return the amount of elements in the list
+        if(army.Count != 0)
+        {
+            foreach(GameObject unit in army)
+            {
+                
+            }
+        }
+        
+    }
+    public int Money
+    {
+        get { return money; }
+        set { money = value; }
+    }
+
+    //Private Variables
+    private List <GameObject> army;
+    private int money;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlaceUnit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
