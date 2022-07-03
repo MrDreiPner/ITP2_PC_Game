8611caed3836f5b7f98111a34be93fe43bbb38f0
Highlander0815 <o.mueller@outlook.at>
2022-07-03 16:05:05 +0200

    Tooltips added

---
+++ Unity_Main/Assets/myAssets/Scripts/Tooltip.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode()]
+public class Tooltip : MonoBehaviour
+{
+    public TextMeshProUGUI headerField;
+
+    public TextMeshProUGUI contentField;
+
+    public LayoutElement layoutElement;
+
+    public int characterWrapLimit;
+
+
+
+    public void SetText(string content, string header = "")
+    {
+        if (string.IsNullOrEmpty(header))
+        {
+            headerField.gameObject.SetActive(false);
+        }
+        else
+        {
+            headerField.gameObject.SetActive(true);
+            headerField.text = header;
+        }
+        contentField.text = content;
+
+        int headerLength = headerField.text.Length;
+        int contentLength = contentField.text.Length;
+
+        layoutElement.enabled = (headerLength > characterWrapLimit || contentLength > characterWrapLimit) ? true : false;
+    }
+
+    private void Update()
+    {
+        if (Application.isEditor)
+        {
+            int headerLength = headerField.text.Length;
+            int contentLength = contentField.text.Length;
+
+            layoutElement.enabled = (headerLength > characterWrapLimit || contentLength > characterWrapLimit) ? true : false;
+        }
+
+        Vector2 position = Input.mousePosition;
+        transform.position = position;
+    }
+}
