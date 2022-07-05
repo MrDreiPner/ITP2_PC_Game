36917d568b57ddfaf8b5b8db740e9a9bb4441eb1
Highlander0815 <o.mueller@outlook.at>
2022-07-05 18:05:19 +0200

    Added Comments to Tooltip Scripts

--- Unity_Main/Assets/myAssets/Scripts/Tooltip.cs
+++ Unity_Main/Assets/myAssets/Scripts/Tooltip.cs
@@ -5,23 +5,30 @@ using UnityEngine.EventSystems;
 
 public class Tooltip : MonoBehaviour
 {
+    //Input fields to be set individually in Unity for each object
     public string header;
+    //textarea, so that the content of tooltips can be easily multilined
     [TextArea]
     public string content;
+    //time to wait for displaying the tooltip; wait 500ms while hovering with mouse over object until tooltip is displayed
     private float timetowait = 0.5f;
 
+
+    //when mouse hovers over object, stop all coroutines(timers) and start the coroutine Starttimer
     private void OnMouseEnter()
     {
         StopAllCoroutines();
         StartCoroutine(StartTimer());
     }
 
+    //when mouse leaves an object, stop all coroutines and call hidetooltip method
     private void OnMouseExit()
     {
         StopAllCoroutines();
         TooltipManager._instance.HideToolTip();
     }
 
+    //StartTimer method which waits 500ms and then calls SetAndShowToolTip Method with header and content parameters
     private IEnumerator StartTimer()
     {
         yield return new WaitForSeconds(timetowait);
--- Unity_Main/Assets/myAssets/Scripts/TooltipManager.cs
+++ Unity_Main/Assets/myAssets/Scripts/TooltipManager.cs
@@ -7,11 +7,11 @@ public class TooltipManager : MonoBehaviour
 {
     public static TooltipManager _instance;
 
+    //Input fields to be set individually in Unity for each object
     public TextMeshProUGUI headerfield;
     public TextMeshProUGUI contentfield;
 
-    public int characterWrapLimit;
-
+    //When waking up destroy different existing instance or create set new one
     private void Awake()
     {
         if(_instance != null && _instance != this)
@@ -24,19 +24,20 @@ public class TooltipManager : MonoBehaviour
         }
     }
 
-    // Start is called before the first frame update
+    // Start is called before the first frame update; set cursor to visible and set tooltip inactive
     void Start()
     {
         Cursor.visible = true;
         gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
+    // Update is called once per frame; Lets the tooltip follow the mouse
     void Update()
     {
         transform.position = Input.mousePosition;
     }
 
+    //Gets header and content filled with data, sets toolbox to active and fills it with the passed data
     public void SetAndShowToolTip(string header, string content)
     {
         gameObject.SetActive(true);
@@ -44,6 +45,7 @@ public class TooltipManager : MonoBehaviour
         contentfield.text = content;
     }
 
+    //set toolbox inactive and set text of it to empty strings
     public void HideToolTip()
     {
         gameObject.SetActive(false);
